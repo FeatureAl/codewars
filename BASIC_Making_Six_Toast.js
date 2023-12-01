f348ee37ed685344411d6252f7957e8f65257e1d
FeatureAl <alenfront@gmail.com>
2023-12-02 00:42:19 +0700

    BASIC: Making Six Toast

---
+++ BASIC_Making_Six_Toast.js
@@ -0,0 +1,17 @@
+/*Story:
+You are going to make toast fast, you think that you should make multiple pieces of toasts and once. So, you try to make 6 pieces of toast.
+
+Problem:
+You forgot to count the number of toast you put into there, you don't know if you put exactly six pieces of toast into the toasters.
+
+Define a function that counts how many more (or less) pieces of toast you need in the toasters. Even though you need more or less, the number will still be positive, not negative. */
+
+
+//Solution
+
+function sixToast(num) {
+    if (num < 6) {
+      return 6 - num
+    } else 
+      return num - 6;
+  }
